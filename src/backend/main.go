b8dd45e0a9bdb92a56cbe1e34f1808c18ac090dc
justin <limjustin2@gmail.com>
2021-05-28 12:24:59 -0700

    Changes

--- src/backend/main.go
+++ src/backend/main.go
@@ -1,8 +1,26 @@
 // Placeholder for main file
 package main
 
-import ()
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
+	addr := os.Getenv("ADDR")
+	if len(addr) == 0 {
+		addr = ":443"
+	}
 
+	tlsKeyPath := os.Getenv("TLSKEY")
+	tlsCertPath := os.Getenv("TLSCERT")
+	if tlsKeyPath == "" || tlsCertPath == "" {
+		log.Fatal("Environrment not set")
+	}
+	mux := mux.NewRouter()
+
+	log.Fatal(http.ListenAndServeTLS(addr, tlsCertPath, tlsKeyPath, mux))
 }
