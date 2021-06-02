fbe2f674c1c441884b10c189d7cccf192fa60b76
justin <limjustin2@gmail.com>
2021-06-02 14:08:27 -0700

    Merge

--- src/backend/main.go
+++ src/backend/main.go
@@ -4,29 +4,11 @@ import (
 	"log"
 	"net/http"
 	"os"
-<<<<<<< HEAD
-
-	"github.com/gorilla/mux"
-=======
->>>>>>> 07c50e2feae86c7c5640e00f51ef0ffb64a23de3
 )
 
 func main() {
 	addr := os.Getenv("ADDR")
 	if len(addr) == 0 {
-<<<<<<< HEAD
-		addr = ":443"
-	}
-
-	tlsKeyPath := os.Getenv("TLSKEY")
-	tlsCertPath := os.Getenv("TLSCERT")
-	if tlsKeyPath == "" || tlsCertPath == "" {
-		log.Fatal("Environrment not set")
-	}
-	mux := mux.NewRouter()
-
-	log.Fatal(http.ListenAndServeTLS(addr, tlsCertPath, tlsKeyPath, mux))
-=======
 		addr = ":80" // TODO: Change to 443 When implementing TLS
 	}
 
@@ -37,9 +19,9 @@ func main() {
 	log.Fatal(http.ListenAndServe(addr, mux))
 }
 
-func getEnv(name string) string{
+func getEnv(name string) string {
 	result := os.Getenv(name)
-	if (len(result) == 0) {
+	if len(result) == 0 {
 		envNotFound(name)
 	}
 	return result
@@ -47,5 +29,4 @@ func getEnv(name string) string{
 
 func envNotFound(name string) {
 	log.Fatalf("%s not set or not found", name)
->>>>>>> 07c50e2feae86c7c5640e00f51ef0ffb64a23de3
 }
